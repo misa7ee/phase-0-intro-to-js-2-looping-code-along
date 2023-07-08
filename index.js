7309afce517b73cb114e7ded5f32a4d5c5be1e13
Sajiya <misa7ee@gmail.com>
2023-07-08 16:52:24 -0400

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+const messages = []
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+
+let number = 10;
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+      (number--)
+}}
+
